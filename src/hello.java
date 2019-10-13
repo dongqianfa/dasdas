05b73d7f0fe67a26baa2ee72f219297d69d42595
dongqianfa <dongqianfa@hotmail.com>
2019-10-12 23:58:54 -0400

    这是分支1的第一次提交

--- src/hello.java
+++ src/hello.java
@@ -3,7 +3,6 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("world");
-
-        System.out.println("\t");
+        System.out.println("这是新分支中添加的代码..");
     }
 }
